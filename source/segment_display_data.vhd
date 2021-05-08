24bc6962ae9906636463deb8b39dd1cf96fa427b
Justas Furmonas <JustasFurmonas@gmail.com>
2021-05-08 10:36:32 +0300

    7 segment display done (max values - 3FF)

--- source/segment_display_data.vhd
+++ source/segment_display_data.vhd
@@ -14,21 +14,55 @@ entity segment_display_data is
 end segment_display_data;
 
 architecture behavioral of segment_display_data is
-    alias seg1 : std_logic_vector(3 downto 0) is i_data(PORT_NUM-1 downto PORT_NUM-4);
-    alias seg2 : std_logic_vector(3 downto 0) is i_data(PORT_NUM-5 downto PORT_NUM-8);
-    alias seg3 : std_logic_vector(1 downto 0) is i_data(PORT_NUM-9 downto PORT_NUM-10);
+    alias seg1 : std_logic_vector(1 downto 0) is i_data(PORT_NUM-1 downto PORT_NUM-2);
+    alias seg2 : std_logic_vector(3 downto 0) is i_data(PORT_NUM-3 downto PORT_NUM-6);
+    alias seg3 : std_logic_vector(3 downto 0) is i_data(PORT_NUM-7 downto PORT_NUM-10);
     begin
     
     with seg1 select
-        o_disp1 <= "0000000" when "0000",
+        o_disp1 <= "1000000" when "00",
+                   "1111001" when "01",
+                   "0100100" when "10",
+                   "0110000" when "11",
                    "1111111" when others;
+        
                    
     with seg2 select
-        o_disp2 <= "0000000" when "0000",
+        o_disp2 <= "1000000" when "0000",
+                   "1111001" when "0001",
+                   "0100100" when "0010",
+                   "0110000" when "0011",
+                   "0011001" when "0100",
+                   "0010010" when "0101",
+                   "0000010" when "0110",
+                   "1111000" when "0111",
+                   "0000000" when "1000",
+                   "0010000" when "1001",
+                   "0001000" when "1010",
+                   "0000011" when "1011",
+                   "1000110" when "1100",
+                   "0100001" when "1101",
+                   "0000110" when "1110",
+                   "0001110" when "1111",
                    "1111111" when others;
                    
     with seg3 select
-        o_disp3 <= "0000000" when "00",
+        o_disp3 <= "1000000" when "0000",
+                   "1111001" when "0001",
+                   "0100100" when "0010",
+                   "0110000" when "0011",
+                   "0011001" when "0100",
+                   "0010010" when "0101",
+                   "0000010" when "0110",
+                   "1111000" when "0111",
+                   "0000000" when "1000",
+                   "0010000" when "1001",
+                   "0001000" when "1010",
+                   "0000011" when "1011",
+                   "1000110" when "1100",
+                   "0100001" when "1101",
+                   "0000110" when "1110",
+                   "0001110" when "1111",
                    "1111111" when others;
         
 end behavioral;
